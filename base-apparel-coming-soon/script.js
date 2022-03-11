5c04ea9edf50cb30e381945223884de173c3f3cd
Nicole <ngott30@gmail.com>
2022-03-11 11:18:32 +0100

    embedded script.js

---
+++ base-apparel-coming-soon/script.js
@@ -0,0 +1,12 @@
+/*=========================================================================
+Script for Frontend-Mentor Challenge Base Apparel Coming Soon 03/22
+
+file: script.js
+date: 11.03.2022
+author: Nicole Gottschall
+
+This script includes hover states for all interactive elements of the page.
+Furthermore it enables the user to receive a error message when the form is
+submitted, but the input field is empty or email address is not formatted
+correctly.
+==========================================================================*/
