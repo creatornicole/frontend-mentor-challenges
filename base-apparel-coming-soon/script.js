2bb9f8eb6a3fec6d73a3d16d93e457540a791960
Nicole <ngott30@gmail.com>
2022-03-11 11:46:12 +0100

    added hover effect

--- base-apparel-coming-soon/script.js
+++ base-apparel-coming-soon/script.js
@@ -10,3 +10,35 @@ Furthermore it enables the user to receive a error message when the form is
 submitted, but the input field is empty or email address is not formatted
 correctly.
 ==========================================================================*/
+
+
+
+
+
+/*=========================================================================
+preparation
+==========================================================================*/
+/* get needed elements */
+btn = document.querySelector('.submit');
+
+
+/*=========================================================================
+hover event
+==========================================================================*/
+btn.addEventListener("mouseover", function() {
+  btn.style.cssText = `
+    cursor: pointer;
+    background: url(images/icon-arrow.svg), var(--desaturated-red);
+    background-repeat: no-repeat;
+    background-position: center;
+    box-shadow: 2px 12px 25px var(--desaturated-red);
+  `;
+});
+
+btn.addEventListener("mouseout", function() {
+  btn.style = "inherit";
+});
+
+/*=========================================================================
+error message (error occurs when user submits form with false input fields)
+==========================================================================*/
